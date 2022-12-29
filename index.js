32391f0ad44b952f3346de843a191cfabf050869
Saul Roldan <saulry90@gmail.com>
2022-12-29 21:56:21 +0100

    array yomismo

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
+
+const yomismo = [
+    "Saúl",
+    32,
+    true,
+    "05/06/1990",
+    { serie: { titulo: "Black Mirror", anio: 2011, genero: "ciencia ficción" } }
+]
